98c8630eb8351f4d9287d174a0b159db58c8b96d
xiaojun gao <gaoxiaojun@gmail.com>
2014-10-10 10:35:30 +0800

    4

--- src/app/utils.h
+++ src/app/utils.h
@@ -33,7 +33,7 @@
 #include <QByteArray>
 #include <QString>
 
-const char APPLICATION_NAME[] = "Qt Creator Crash Handler";
+const char APPLICATION_NAME[] = "QtRCP Crash Handler";
 const char URL_BUGTRACKER[] = "https://bugreports.qt-project.org/";
 
 QByteArray fileContents(const QString &filePath);
